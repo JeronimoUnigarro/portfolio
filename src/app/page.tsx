2a05625e6474cb0cbc8cda16fa023599c9ce88f2
JeronimoUnigarro <jeronimo.unigarro@campusuccl.edu.co>
2025-03-14 14:58:26 -0500

    Style and color palette update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-cyan-300 flex items-center justify-center">
-      <h1 className="text-cyan-900 text-4xl font-extrabold">Jeronimo Unigarro </h1>
+    <main className="w-screen h-screen bg-one flex items-center justify-center">
+      <h1 className="text-two text-4xl font-extrabold">Jeronimo Unigarro </h1>
     </main>
   );
 }
